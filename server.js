1c8e09179fb7e369fd0b9751bc4033f2798f5c38
fainfante <fainfante8@gmail.com>
2025-05-22 15:09:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(require('./utils/errorHandler'));
 
 // Start
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`
 ┌─────────────────────────────────────────┐
 │                                         │
